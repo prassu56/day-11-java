4c73249f157e707d0ff86cd0e3faece5e684b5cf
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-12 20:31:33 +0530

    Add files via upload

---
+++ Linkedlist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Linkedlist{
+    public static void main(String[]args){
+        LinkedList<Integer> a=new LinkedList<>();
+        a.add(567);
+        a.add(789);
+        a.add(900);
+        System.out.println(a);
+        a.addFirst(89);
+        System.out.println(a);
+        a.addLast(90);
+        System.out.println(a);
+    }
+    
+}
