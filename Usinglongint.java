84b5a64cba4ac1fb9702d4540285516037e4490f
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-12 19:34:43 +0530

    Add files via upload

---
+++ Usinglongint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Usinglongint{
+    public static void main(String[]args){
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
