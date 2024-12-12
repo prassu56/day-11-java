4a071b546c531131c667a60e77b8fc8d7cc3761b
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-12 19:28:58 +0530

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ class Collection{
+    public static void main(String[]args){
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(45);
+        a.add(1);
+        a.add(18);
+        a.add(12);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+}
